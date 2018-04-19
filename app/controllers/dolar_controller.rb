4613a3807b4d101ea2af94e081caf003af77665f
Kenneth Peet <kpeet@inf.utfsm.cl>
2018-04-19 05:13:08 -0300

    último ajuste en visualización TMC

--- app/controllers/dolar_controller.rb
+++ app/controllers/dolar_controller.rb
@@ -17,12 +17,17 @@ class DolarController < ApplicationController
     @titulo="Dolar"
     fecha_inicio_format= params[:fecha_inicio]
     fecha_final_format= params[:fecha_final]
-    @result = Dolar.get_api_value(fecha_inicio_format, fecha_final_format)
-    @data_table_max_min_dolar_value=Dolar.generate_dashboard_value(@result,fecha_inicio_format, fecha_final_format)
-    @tweets_count=Dolar.data_table(@result,fecha_inicio_format, fecha_final_format)
-    @data_bock=Dolar.data_bock(@result,fecha_inicio_format, fecha_final_format)
-    @fecha_inicio=fecha_inicio_format
-    @fecha_final=fecha_final_format
+    if( (fecha_final_format=='' || fecha_inicio_format=='') || (fecha_final_format <  fecha_inicio_format) || (fecha_final_format.split("-").size!=3 || fecha_inicio_format.split("-").size!=3) )
+      @result=[]
+
+    else
+        @result = Dolar.get_api_value(fecha_inicio_format, fecha_final_format)
+        @data_table_max_min_dolar_value=Dolar.generate_dashboard_value(@result,fecha_inicio_format, fecha_final_format)
+        @data_graph=Dolar.data_table(@result,fecha_inicio_format, fecha_final_format)
+        @data_bock=Dolar.data_bock(@result,fecha_inicio_format, fecha_final_format)
+        @fecha_inicio=fecha_inicio_format
+        @fecha_final=fecha_final_format
+    end
   end
 
 end
--- app/controllers/uf_controller.rb
+++ app/controllers/uf_controller.rb
@@ -12,16 +12,23 @@ class UfController < ApplicationController
     @titulo="UF"
     fecha_inicio_format= params[:fecha_inicio]
     fecha_final_format= params[:fecha_final]
-    @result = Uf.get_api_value(fecha_inicio_format, fecha_final_format)
-    if(@result.empty?)
-      @tweets_count=[]
+    if( (fecha_final_format=='' || fecha_inicio_format=='') || (fecha_final_format <  fecha_inicio_format) || (fecha_final_format.split("-").size!=3 || fecha_inicio_format.split("-").size!=3) )
+      @result=[]
+
     else
-      @data_table_max_min_dolar_value=Uf.generate_dashboard_value(@result,fecha_inicio_format, fecha_final_format)
-      @tweets_count=Uf.data_table(@result,fecha_inicio_format, fecha_final_format)
-      @data_bock=Uf.data_bock(@result,fecha_inicio_format, fecha_final_format)
+      @result = Uf.get_api_value(fecha_inicio_format, fecha_final_format)
+      if(@result.empty?)
+        @data_graph=[]
+      else
+        @data_table_max_min_dolar_value=Uf.generate_dashboard_value(@result,fecha_inicio_format, fecha_final_format)
+        @data_graph=Uf.data_table(@result,fecha_inicio_format, fecha_final_format)
+        @data_bock=Uf.data_bock(@result,fecha_inicio_format, fecha_final_format)
+      end
+
+      @fecha_inicio=fecha_inicio_format
+      @fecha_final=fecha_final_format
     end
 
-    @fecha_inicio=fecha_inicio_format
-    @fecha_final=fecha_final_format
+
   end
 end
